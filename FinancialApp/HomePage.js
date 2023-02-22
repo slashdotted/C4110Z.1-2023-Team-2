77ff9f9b3923833909e976132a70e56b9c17e9b0
Sostene <jetw.ncc@gmail.com>
2023-02-22 18:57:58 +0100

    Home page

--- FinancialApp/HomePage.js
+++ FinancialApp/HomePage.js
@@ -1,16 +1,16 @@
 import { Text, Image, View, SafeAreaView } from "react-native";
-import Styles from "./assets/styles/Styles";
+import Styles from "./assets/styles/HomePage";
 
 export default function App() {
   return (
     <SafeAreaView style={Styles.AdroidSafeArea}>
       <Image
-        source={{ uri: "https://picsum.photos/200/300" }}
+        source={{ uri: "https://picsum.photos/150/150" }}
         style={Styles.Logo}
       />
       <Text>User Name</Text>
       <View style={{ flex: 0.7, justifyContent: "center" }}>
-        <Text>Name and Logo of the app</Text>
+        <Text>Logo of the app</Text>
       </View>
       <Text>Tap to continue</Text>
     </SafeAreaView>
