d0ee33adc2365b4034c2d8bdcc23e5566303dc6b
gulivarese <gulivarese@gmail.com>
2023-03-01 11:45:51 +0100

    Aggiungere parte con le foto di gruppo riga 70

--- FinancialApp/assets/styles/WhoWeAre.js
+++ FinancialApp/assets/styles/WhoWeAre.js
@@ -9,15 +9,53 @@ export default StyleSheet.create({
     alignItems: "center",
   },
 
+  Container: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "blue",
+  },
+
   GroupPhoto: {
+    marginTop: 2,
     width: 200,
     height: 100,
     borderRadius: 6,
+    marginBottom: 2,
+  },
+
+  ContainerPhoto: {
+    alignItems: "baseline",
   },
 
   SinglePhoto: {
-    width: 80,
-    height: 80,
+    width: 110,
+    height: 110,
     borderRadius: 20,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+
+  textScrollDown: {
+    textAlign: "center",
+    fontSize: 14,
+    fontWeight: "700",
+  },
+
+  boldText: {
+    fontWeight: "bold",
+    color: "blue",
+    fontSize: 18,
+  },
+
+  name: {
+    fontWeight: "600",
+    fontSize: 15,
+    color: "blue",
+  },
+
+  containerSinglePhoto: {
+    marginBottom: 2,
+    marginTop: 1,
   },
 });
--- FinancialApp/screens/WhoWeAre.js
+++ FinancialApp/screens/WhoWeAre.js
@@ -1,53 +1,72 @@
-import { Text, Image, View, SafeAreaView } from "react-native";
+import { Text, Image, View, SafeAreaView, ScrollView } from "react-native";
 import Styles from "../assets/styles/WhoWeAre";
 
 export default function WhoWeAre({ navigation }) {
   return (
-    <SafeAreaView style={Styles.AdroidSafeArea}>
-      <Text> We are the Team 2</Text>
+    <ScrollView>
+      <SafeAreaView>
+        <View style={Styles.Container}>
+          <Text style={Styles.boldText}> We are Team 2</Text>
+          <Image
+            source={require("../assets/group.jpg")}
+            style={Styles.GroupPhoto}
+          />
+        </View>
 
-      <Image source={require("../assets/1.jpg")} style={Styles.GroupPhoto} />
-      <View style={{ flex: 0.2, justifyContent: "center", width: "50%" }}>
-        <Text>
-          AGGIUNGERE UNA VIEW PER OGNI FOTO COL NOME E RIGA SOTTO OGNI FOTO
-        </Text>
-      </View>
+        <View style={{ alignContent: "center" }}>
+          <Text style={Styles.textScrollDown}>Scroll Down</Text>
+        </View>
 
-      <Image
-        source={require("../assets/Leeann.png")}
-        resizeMode="contain"
-        style={Styles.SinglePhoto}
-      />
+        <View style={Styles.ContainerPhoto}>
+          <View style={Styles.containerSinglePhoto}>
+            <Text style={Styles.name}> LEEAN RUNKLE </Text>
+            <Image
+              source={require("../assets/Leeann.png")}
+              style={Styles.SinglePhoto}
+            ></Image>
+          </View>
 
-      <Image
-        source={require("../assets/Nicholas.png")}
-        resizeMode="contain"
-        style={Styles.SinglePhoto}
-      />
+          <View style={Styles.containerSinglePhoto}>
+            <Text style={Styles.name}> JON WHITBY </Text>
+            <Image
+              source={require("../assets/Jon.png")}
+              style={Styles.SinglePhoto}
+            ></Image>
+          </View>
 
-      <Image
-        source={require("../assets/Jon.png")}
-        resizeMode="contain"
-        style={Styles.SinglePhoto}
-      />
+          <View style={Styles.containerSinglePhoto}>
+            <Text style={Styles.name}> JADEN WEED </Text>
+            <Image
+              source={require("../assets/Jaden.png")}
+              style={Styles.SinglePhoto}
+            ></Image>
+          </View>
 
-      <Image
-        source={require("../assets/Jaden.png")}
-        resizeMode="contain"
-        style={Styles.SinglePhoto}
-      />
+          <View style={Styles.containerSinglePhoto}>
+            <Text style={Styles.name}> NICHOLAS CERA </Text>
+            <Image
+              source={require("../assets/Nicholas.png")}
+              style={Styles.SinglePhoto}
+            ></Image>
+          </View>
 
-      <Image
-        source={require("../assets/Guglielmo.jpeg")}
-        resizeMode="contain"
-        style={Styles.SinglePhoto}
-      />
+          <View style={Styles.containerSinglePhoto}>
+            <Text style={Styles.name}> GUGLIELMO GAGLIARDI </Text>
+            <Image
+              source={require("../assets/Guglielmo.jpeg")}
+              style={Styles.SinglePhoto}
+            ></Image>
+          </View>
 
-      <Image
-        source={require("../assets/Walter.png")}
-        resizeMode="contain"
-        style={Styles.SinglePhoto}
-      />
-    </SafeAreaView>
+          <View style={Styles.containerSinglePhoto}>
+            <Text style={Styles.name}> WALTER SOSTENE LOSA </Text>
+            <Image
+              source={require("../assets/Walter.png")}
+              style={Styles.SinglePhoto}
+            ></Image>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
