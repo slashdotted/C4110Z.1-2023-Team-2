bcb7a3e8da4a2ccb9b760c4ce129b4f0e79ebade
gulivarese <gulivarese@gmail.com>
2023-04-22 00:18:10 +0200

    to do: 4,5° screen theory english part and implement button

---
+++ FinancialApp/screens/EnglishTheory/2.js
@@ -0,0 +1,133 @@
+import Styles from "../assets/styles/Theory";
+
+import {
+  Text,
+  Image,
+  View,
+  SafeAreaView,
+  Button,
+  ScrollView,
+  Alert,
+} from "react-native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+export default function TheoryMenu() {
+  return (
+    <Stack.Navigator
+      initialRouteName="TheoryPage"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="TheoryPage" component={Theory} />
+      <Stack.Screen name="Quiz" component={Quiz} />
+    </Stack.Navigator>
+  );
+}
+
+function Theory({ navigation }) {
+  return (
+    <SafeAreaView style={Styles.Layout}>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Text style={Styles.boldText}>THEORY</Text>
+          <Image
+            source={require("../assets/theoryPart.jpg")}
+            style={Styles.GroupPhoto}
+          />
+        </View>
+        <Text style={Styles.Paragraph}>What is budgeting?</Text>
+        <View>
+          <Text style={Styles.TextParagraph}>
+            Budgeting is the process of estimating personal expenses and income
+            across a future period of time that is then evaluated routinely in
+            order to improve spending habits. A budget refers to the actual plan
+            a person develops that shows how much money they will earn and spend
+            in a specified timeframe. Typically, a budget is set up monthly,
+            however, a person may choose any span of time that works for them.
+            Budgeting allows a person to realize how they are allocating their
+            funds, which, in return, gives them the ability to adjust their
+            spending and save money for necessities.
+          </Text>
+
+          <Text style={Styles.Paragraph}>How to set up a budget?</Text>
+          <Text style={Styles.TextParagraph}>
+            The first step a person must take when creating a budget is
+            calculating the net income for the specified period of time they
+            chose. Net income is a person’s total wages/salary minus any
+            deductions. Common deductions include various taxes, retirement
+            plans, and health insurance. It is crucial for a person to base a
+            budget off of their net income rather than their total income in
+            order to avoid overspending because said person believes they have
+            more money, which in reality is actually lost to deductions. After
+            knowing the amount of money earned in the chosen timeframe, a person
+            needs to uncover records of past expenses throughout the same
+            period. A person should categorize their expenses into fixed
+            expenses and variable expenses. Fixed expenses are those that do not
+            change between periods of time; Such as, housing rent or mortgage,
+            car payments, and insurance payments. Fixed expenses often are for
+            necessities and must be paid on time within the period. Variable
+            expenses describe those that vary across different spans of time;
+            Such as, gas for an automobile, utilities, groceries, and
+            entertainment. A person should base their variable expenses for a
+            given period on the amount they spent on the category for the same
+            period the previous year. This information can be found in credit
+            card and bank statements, which already itemize the charges. These
+            expenses tend to classify more as “wants” and not “needs” and
+            provide a great starting place to reduce spending.
+          </Text>
+          <Text style={Styles.TextParagraph}>
+            Once a person gathers their financial information for the specified
+            timeframe, they can determine realistic goals and create a plan to
+            achieve them. First, a person will analyze the money they will earn
+            across the period, as well as how much and in which ways they spent
+            their money in the period from the previous year, and set certain
+            financial goals to reach at the end of the timespan. These goals
+            might include paying credit card debt to a certain extent, building
+            up an emergency fund, or saving money towards a vacation. A person
+            then develops a plan which places specific limits on each category
+            of expense in a way that pursues the chosen goals. One popular
+            budgeting technique is to utilize the 50/30/20 rule, in which fifty
+            percent of your earnings should be allocated to necessities (rent),
+            thirty percent should be allocated to your desires (entertainment),
+            and twenty percent should be allocated to savings. This plan is
+            referred to as the person’s budget.
+          </Text>
+
+          <Text style={Styles.Paragraph}>How to use a budget?</Text>
+          <Text style={Styles.TextParagraph}>
+            Now that the budget is formed, a person must track their spending
+            throughout the stretch of time, recording the amount and destination
+            of each expense. The person then needs to periodically compare their
+            spending records to their budget, ensuring that their actions align
+            with their plan. In doing so, it is easy for that person to realize
+            if they are straying away from their financial goals and adjust
+            their spending accordingly. As mentioned before, a person should
+            look to reduce certain variable expenses that they notice do not
+            hold precedent over other necessities. If a person cannot afford to
+            reduce variable expenses further, and they are still not able to
+            meet expected expenses, they should consider changing fixed
+            expenses, so as to reduce them as well. This can include moving to a
+            more affordable place of residence or switching insurance plans.
+          </Text>
+
+          <Text style={Styles.Paragraph}>The purpose of budgeting </Text>
+          <Text style={Styles.TextParagraph}>
+            Budgeting allows a person to easily recognize and modify their
+            spending habits, which in turn, will lead them to develop a more
+            effective way to reach their financial goals and pay off expenses.
+            Using a budget will help a person to possess money for necessities
+            and save money that they thought they could not. Over time, these
+            savings will grow and will allow people to reach financial goals
+            that will ultimately bring them more satisfaction in their lives,
+            whether or not that entails adding more financial stability or more
+            entertainment for them.
+          </Text>
+        </View>
+
+        <Button title="QUIZ" onPress={() => navigation.navigate("Quiz")} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
---
+++ FinancialApp/screens/EnglishTheory/3.js
@@ -0,0 +1,130 @@
+import Styles from "../assets/styles/Theory";
+
+import {
+  Text,
+  Image,
+  View,
+  SafeAreaView,
+  Button,
+  ScrollView,
+  Alert,
+} from "react-native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+export default function TheoryMenu() {
+  return (
+    <Stack.Navigator
+      initialRouteName="TheoryPage"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="TheoryPage" component={Theory} />
+      <Stack.Screen name="Quiz" component={Quiz} />
+    </Stack.Navigator>
+  );
+}
+
+function Theory({ navigation }) {
+  return (
+    <SafeAreaView style={Styles.Layout}>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Text style={Styles.boldText}>THEORY</Text>
+          <Image
+            source={require("../assets/theoryPart.jpg")}
+            style={Styles.GroupPhoto}
+          />
+        </View>
+        <View style={Styles.Separator}>
+          <Text style={Styles.Paragraph}>Filing Taxes</Text>
+          <View>
+            <Text style={Styles.TextParagraph}>
+              In the United States, filing taxes can feel like an overly
+              complicated and stressful task. The forms and calculations
+              involved are overwhelming at first, and the last thing you want is
+              to face legal trouble for getting something wrong. Let’s walk
+              through five basic lessons to help you get organized and relieve
+              stress over filing taxes.
+            </Text>
+
+            <Text style={Styles.Paragraph}>Gathering Documents</Text>
+            <Text style={Styles.TextParagraph}>
+              To begin filing your taxes, it’s important to locate all required
+              documents. These include your W2 or 1099, which are reports of
+              your income, as well as statements of income from investments,
+              interest on a mortgage, or charitable contributions you made. It’s
+              also important to consider any major changes that might need to be
+              indicated on a form, including if you moved, changed your name, or
+              had a major change to your household income. For a full checklist
+              of documents you need to find, click here. You should’ve gotten
+              these documents throughout the year from your employer, bank,
+              credit union, etc. – make sure to keep careful track of documents
+              as they arrive, to make it easier on yourself come tax season!
+            </Text>
+
+            <Text style={Styles.Paragraph}>Choosing Deduction Type</Text>
+            <Text style={Styles.TextParagraph}>
+              A deduction refers to any item that you subtract from the total
+              amount of income that you’ll have to pay taxes on. For instance,
+              if you made $40,000 this year, but deducted $5,000, then only the
+              remaining $35,000 will be taxed – which means that you save money!
+              You can choose to either use the standard or itemized deduction.
+              Standard deduction is a set amount of money that the government
+              sets, which you can use to avoid the hassle of itemizing. Itemized
+              deduction is where you take the time to find and record every item
+              eligible to be deducted. This way is definitely extra work, but it
+              can save you more money if the items total up to more than the
+              standard deduction amount. You can deduct items like childcare,
+              education costs, medical expenses, and charitable donations. Make
+              sure you have proof of anything you write off, in the form of
+              receipts or statements. Again, try to keep these documents
+              organized along the way to avoid extra trouble at tax time.
+            </Text>
+
+            <Text style={Styles.Paragraph}>Picking a Filing Status </Text>
+            <Text style={Styles.TextParagraph}>
+              When filing, it’s important to identify your status, as this
+              determines different steps of the filing process and the amount
+              that you’ll owe. There are five statuses: single, married filing
+              jointly, married filing separately, head of household, and
+              qualifying widow(er). You can read full descriptions [here]. The
+              tricky part is that sometimes you could qualify for different
+              statuses – for instance, maybe you’re a recent widow, and are now
+              single. It’s important to decide which status is most beneficial
+              for you to save the most money, so make sure to compare the
+              numbers for both in this situation.
+            </Text>
+
+            <Text style={Styles.Paragraph}>Filing your Taxes </Text>
+            <Text style={Styles.TextParagraph}>
+              Once you have everything organized, it’s time to file! You can
+              file your taxes yourself online or by mail, or you can hire a tax
+              pro to help you file. Filing yourself will save you money and can
+              be simple if you’re using the standard deduction (see Lesson 2).
+              If you want to itemize deductions, or you have a special situation
+              (ex: owning your own business with employees), then it may be
+              worth paying a professional.
+            </Text>
+
+            <Text style={Styles.Paragraph}>Looking Ahead to Next Year </Text>
+            <Text style={Styles.TextParagraph}>
+              After you file, you’ll either get a refund or a bill. Basically,
+              the government will look at how much you’ve been withholding from
+              your income all year, and compare it to the information you filed,
+              then either pay you back (refund) or charge you extra (bill) if
+              the two aren’t matching up. To prepare for next year, you can
+              adjust how much you’re withholding from your income based on your
+              refund (withhold less) or your bill (withhold more). Also, make
+              sure to hang on to any documents you used in filing this year, in
+              case you need them next year, or in case you’re asked to prove
+              anything from this year!
+            </Text>
+          </View>
+        </View>
+        <Button title="QUIZ" onPress={() => navigation.navigate("Quiz")} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
